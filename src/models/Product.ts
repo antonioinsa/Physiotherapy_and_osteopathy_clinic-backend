726512dc8470f6d0371e43432bf62900d1de5027
Toni <antonioinsa@tutanota.com>
2023-12-08 23:39:45 +0100

    Feat:Update Products  model and add relations

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,5 +1,33 @@
-import { BaseEntity, Entity } from "typeorm";
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Orderproduct } from "./Orderproduct";
 
 
 @Entity("products")
-export class Product extends BaseEntity {}
+export class Product extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    article!: string
+
+    @Column()
+    description!: string
+
+    @Column({ type: "decimal", precision: 5, scale: 2 })
+    price!: number
+
+    @Column()
+    stock!: string
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    created_at!: Date
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updated_at!: Date
+
+
+    @OneToMany(() => Orderproduct, (product) =>
+        product.productOrderProduct)
+        products!: Orderproduct[]
+}
