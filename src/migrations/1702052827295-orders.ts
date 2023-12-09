5d56d38d746a5c96912d95946200e7d387e53726
Toni <antonioinsa@tutanota.com>
2023-12-09 14:13:09 +0100

    Feat:Create columns in Orders table

--- src/migrations/1702052827295-orders.ts
+++ src/migrations/1702052827295-orders.ts
@@ -1,11 +1,59 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
 export class Orders1702052827295 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "orders",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "user_id",
+                        type: "int",
+                    },
+                    {
+                        name: "status",
+                        type: "enum",
+                        enum: [
+                            "Pending",
+                            "Accepted",
+                            "Completed"
+                        ]
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                        onUpdate: "CURRENT_TIMESTAMP",
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ["user_id"],
+                        referencedTableName: "users",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE",
+                    },
+                ],
+            }),
+            true
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("orders")
     }
 
 }
