2b41e332ef3f930893341e0ffcdd8d59881f6119
Toni <antonioinsa@tutanota.com>
2023-12-09 14:17:42 +0100

    Feat:Create columns in Orderproducts table

--- src/migrations/1702052840296-orderproduct.ts
+++ src/migrations/1702052840296-orderproduct.ts
@@ -1,11 +1,70 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
 export class Orderproduct1702052840296 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "orderproducts",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "order_id",
+                        type: "int",
+                    },
+                    {
+                        name: "product_id",
+                        type: "int",
+                    },
+                    {
+                        name: "quantity",
+                        type: "int",
+                    },
+                    {
+                        name: "price",
+                        type: "decimal",
+                        precision: 4,
+                        scale: 2,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                        onUpdate: "CURRENT_TIMESTAMP",
+                    },
+                ],
+                foreignKeys: [
+                    {
+                        columnNames: ["order_id"],
+                        referencedTableName: "orders",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE",
+                    },
+                    {
+                        columnNames: ["product_id"],
+                        referencedTableName: "products",
+                        referencedColumnNames: ["id"],
+                        onDelete: "CASCADE",
+                    },
+                ],
+            }),
+            true
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("orderproducts")
     }
 
 }
