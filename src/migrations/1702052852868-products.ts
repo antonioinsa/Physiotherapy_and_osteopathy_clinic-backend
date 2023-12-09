4a4f3225a3109fe2f3ee38edd3deabfb51cc7d18
Toni <antonioinsa@tutanota.com>
2023-12-09 13:30:19 +0100

    Feat:Create columns in product table

--- src/migrations/1702052852868-products.ts
+++ src/migrations/1702052852868-products.ts
@@ -1,11 +1,60 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
 export class Products1702052852868 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "products",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "article",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "price",
+                        type: "decimal",
+                        precision: 5,
+                        scale: 2,
+                    },
+                    {
+                        name: "stock",
+                        type: "varchar",
+                        length: "10",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                        onUpdate: "CURRENT_TIMESTAMP",
+                    },
+                ],
+            }),
+            true
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("products")
     }
 
 }
+
