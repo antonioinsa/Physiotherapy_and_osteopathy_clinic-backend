8028d5b176af02a14bfc1124e94b1d915ead2db5
Toni <antonioinsa@tutanota.com>
2023-12-09 13:59:46 +0100

    Feat:Create columns in exercises table

--- src/migrations/1702052685809-exercises.ts
+++ src/migrations/1702052685809-exercises.ts
@@ -1,11 +1,59 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
 
 export class Exercises1702052685809 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "exercises",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                    },
+                    {
+                        name: "activity",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "type",
+                        type: "enum",
+                        enum: [
+                            "Flexibility",
+                            "Tendonitis",
+                            "Neuropahty",
+                            "Cervical",
+                            "Hyperkyphosis",
+                        ],
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "CURRENT_TIMESTAMP",
+                        onUpdate: "CURRENT_TIMESTAMP",
+                    },
+                ],
+            }),
+            true
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("exercises")
     }
 
 }
